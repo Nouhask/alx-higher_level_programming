fc4b661eb991b11958943b092422b19643481c8b
nouhask <nouhaylaskilat@gmail.com>
2024-01-06 03:02:38 -0800

    py data

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list1 = list(tuple_a)
+    list2 = list(tuple_b)
+    for i in range(2):
+        list1.append(0)
+        list2.append(0)
+    sum_0 = list1[0] + list2[0]
+    sum_1 = list1[1] + list2[1]
+    tuple_c = (sum_0, sum_1)
+    return tuple_c
