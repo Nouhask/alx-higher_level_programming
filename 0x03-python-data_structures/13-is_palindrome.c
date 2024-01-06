dcb5780ae0e7e9891f68a79d72a38bf6e5a9cfcc
nouhask <nouhaylaskilat@gmail.com>
2024-01-06 05:47:52 -0800

    py data

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,24 +5,27 @@
  * @head: ptr to the beginning of the list
  * Return: 0 if not palindrome else 1
  */
-int is_palindrome(listint_t **head) 
+int is_palindrome(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return (1);
-    return (aux_palind(head, *head));
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (check_pal(head, *head));
 }
 
 /**
- * aux_palind - function to check if the list is palindrome
+ * check_pal - function to check if the list is palindrome
  * @head: ptr to the beginning of the list
- * @end: ptr to the end of the list
+ * @last: ptr to the end of the list
  * Return: 0 if not palindrom else 1
  */
-int aux_palind(listint_t **head, listint_t *end)
+int check_pal(listint_t **head, listint_t *last)
 {
-    if (end == NULL)
-        return (1);
-    if (aux_palind(head, end->next) && (head)->n == end->n)
-        return (1);
-    return (0);
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
