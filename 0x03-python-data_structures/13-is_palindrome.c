62fd5682c9f8f3156f907938ba52ab8d76dd4fad
nouhask <nouhaylaskilat@gmail.com>
2024-01-06 06:20:51 -0800

    py data

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,20 +9,20 @@ int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (check_pal(head, *head));
+	return (verify_pal(head, *head));
 }
 
 /**
- * check_pal - function to check if the list is palindrome
+ * verify_pal - function to check if the list is palindrome
  * @head: ptr to the beginning of the list
  * @last: ptr to the end of the list
  * Return: 0 if not palindrom else 1
  */
-int check_pal(listint_t **head, listint_t *last)
+int verify_pal(listint_t **head, listint_t *last)
 {
 	if (last == NULL)
 		return (1);
-	if (check_pal(head, last->next) && (*head)->n == last->n)
+	if (verify_pal(head, last->next) && (*head)->pp == last->pp)
 	{
 		*head = (*head)->next;
 		return (1);
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -24,5 +24,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int check_pal(listint_t **head, listint_t *last);
+int verify_pal(listint_t **head, listint_t *last);
 #endif /* LISTS_H */
