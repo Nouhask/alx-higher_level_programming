d8803cb638801cedb041eac79895ae96f4e59ae4
nouhask <nouhaylaskilat@gmail.com>
2024-01-10 04:33:12 -0800

    more data

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def best_score(my_dictionary):
-    if my_dict and len(my_dictionary):
-        max = list(my_dictionary.keys())[0]
-        for key in my_dictionary:
-            if my_dictionary[key] > my_dictionary[max]:
-                max = key
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for cle in my_dict:
+            if my_dict[cle] > my_dict[max]:
+                max = cle
         return max
     return None
