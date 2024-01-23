299bed903c70066edeae156615ef38a55fde868b
nouhask <nouhaylaskilat@gmail.com>
2024-01-23 14:17:24 -0800

    pyclasse

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define a class Square
+"""
+
+
+class Square:
+    """
+    Represent the square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square
+        Args:
+            size (int): size of a new square
+        """
+        self.__size = size
